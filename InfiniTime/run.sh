021b96399f91d45ffea7483148e155060dbde5a8
pasyn <67178218+pasyn@users.noreply.github.com>
2022-10-06 21:31:02 -0400

    Delete InfiniTime directory

--- InfiniTime/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export LD_LIBRARY_PATH=/usr/local/lib64
-export LANG=C
-PATH="/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin"
-CONFIG_PATH=/data/options.json
-echo "infinitime addon"
-
-
